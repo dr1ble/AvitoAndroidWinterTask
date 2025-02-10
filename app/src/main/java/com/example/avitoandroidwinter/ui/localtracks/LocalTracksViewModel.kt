63e2cc762769a84346fd1e40ec3533dc4158de6c
Nikita Popov <nikita.popovv@list.ru>
2025-02-10 20:23:04 +0300

    Basic navigation

--- app/src/main/java/com/example/avitoandroidwinter/ui/localtracks/LocalTracksViewModel.kt
+++ app/src/main/java/com/example/avitoandroidwinter/ui/localtracks/LocalTracksViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class LocalTracksViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is Local Tracks Fragment"
     }
     val text: LiveData<String> = _text
 }
